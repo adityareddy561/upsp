47595add654f0ce51dc1290ae716a40941e850c0
Aditya Reddy Matta <adityareddy561@gmail.com>
2023-06-19 20:53:17 -0500

    fix: Added modal

---
+++ src/main/java/com/upspapp/modal/User.java
@@ -0,0 +1,140 @@
+package com.upspapp.modal;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.upspapp.config.CustomDateAndTimeDeserialize;
+import com.upspapp.config.CustomJsonDateSerializer;
+import com.upspapp.constants.Constants;
+
+@Entity(name = Constants.USER_TABLE_NAME)
+@JsonIgnoreProperties
+public class User implements Serializable {
+
+	public static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@JsonSerialize(using = CustomJsonDateSerializer.class)
+	@JsonDeserialize(using = CustomDateAndTimeDeserialize.class)
+	private Date createdAt;
+
+	@Column(nullable = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@JsonSerialize(using = CustomJsonDateSerializer.class)
+	@JsonDeserialize(using = CustomDateAndTimeDeserialize.class)
+	private Date updatedAt;
+
+	private String fullName;
+	private String email;
+	private String password;
+	private String mobileNumber;
+	private Integer role;
+	private Boolean active = Boolean.TRUE;
+	
+	private String profileImage;
+
+	public String getProfileImage() {
+		return profileImage;
+	}
+
+	public void setProfileImage(String profileImage) {
+		this.profileImage = profileImage;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getRole() {
+		return role;
+	}
+
+	public void setRole(Integer role) {
+		this.role = role;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+}
