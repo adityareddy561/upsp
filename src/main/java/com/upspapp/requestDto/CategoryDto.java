830fa63ad0d3de8285b8f45d5633de5d561a62d8
SanjanaViswanathuni <bviswan@ilstu.edu>
2023-06-20 22:27:50 -0500

    Feat:category

---
+++ src/main/java/com/upspapp/requestDto/CategoryDto.java
@@ -0,0 +1,15 @@
+package com.upspapp.requestDto;
+
+public class CategoryDto {
+
+	private String categoryName;
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+}
---
+++ src/main/java/com/upspapp/requestDto/SubCategoryDto.java
@@ -0,0 +1,24 @@
+package com.upspapp.requestDto;
+
+public class SubCategoryDto {
+
+	private long categoryId;
+	private String subCategoryName;
+
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getSubCategoryName() {
+		return subCategoryName;
+	}
+
+	public void setSubCategoryName(String subCategoryName) {
+		this.subCategoryName = subCategoryName;
+	}
+
+}
