20f3e45da60abbd60f6715f4437c889bbd0c4ec9
Nithisha <sainithisha99@gmail.com>
2023-06-21 20:27:40 -0500

    Fix:Custom Mapper

--- src/main/java/com/upspapp/customMapper/CustomMapper.java
+++ src/main/java/com/upspapp/customMapper/CustomMapper.java
@@ -3,8 +3,12 @@ package com.upspapp.customMapper;
 import org.mapstruct.Mapper;
 
 import com.upspapp.modal.Buyer;
+import com.upspapp.modal.Category;
 import com.upspapp.modal.Seller;
+import com.upspapp.modal.SubCategory;
 import com.upspapp.modal.User;
+import com.upspapp.requestDto.CategoryDto;
+import com.upspapp.requestDto.SubCategoryDto;
 import com.upspapp.requestDto.UserDto;
 
 @Mapper(componentModel = "spring")
@@ -12,6 +16,10 @@ public interface CustomMapper {
 
 
 	User userDtoToUser(UserDto dto);
+
+	Category categoryDtoToCategory(CategoryDto dto);
+
+	SubCategory subCategoryDtoToSubCategory(SubCategoryDto dto);
 
 	Buyer userDtoToBuyer(UserDto userDto);
 
