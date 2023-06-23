2c246879671179d4fe1e83afbadd7b4e07802a4d
Nithisha <sainithisha99@gmail.com>
2023-06-23 15:07:24 -0500

    Fix: customMapper

--- src/main/java/com/upspapp/customMapper/CustomMapper.java
+++ src/main/java/com/upspapp/customMapper/CustomMapper.java
@@ -1,16 +1,12 @@
 package com.upspapp.customMapper;
 
 import org.mapstruct.Mapper;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 
 import com.upspapp.modal.Advertisement;
 import com.upspapp.modal.Buyer;
 import com.upspapp.modal.Category;
 import com.upspapp.modal.Feedback;
 import com.upspapp.modal.PostLike;
-import com.upspapp.modal.Offer;
-import com.upspapp.modal.Rating;
-import com.upspapp.modal.Report;
 import com.upspapp.modal.PostSave;
 import com.upspapp.modal.Seller;
 import com.upspapp.modal.SubCategory;
@@ -19,9 +15,6 @@ import com.upspapp.requestDto.AdvertisementDto;
 import com.upspapp.requestDto.CategoryDto;
 import com.upspapp.requestDto.FeedbackDto;
 import com.upspapp.requestDto.PostLikeDto;
-import com.upspapp.requestDto.OfferDto;
-import com.upspapp.requestDto.RatingDto;
-import com.upspapp.requestDto.ReportDto;
 import com.upspapp.requestDto.PostSaveDto;
 import com.upspapp.requestDto.SubCategoryDto;
 import com.upspapp.requestDto.UserDto;
@@ -37,8 +30,6 @@ public interface CustomMapper {
 	Category categoryDtoToCategory(CategoryDto dto);
 
 	SubCategory subCategoryDtoToSubCategory(SubCategoryDto dto);
-
-	Report reportDtoToReport(ReportDto dto);
 
 	Feedback feedbackDtoToFeedback(FeedbackDto dto);
 
