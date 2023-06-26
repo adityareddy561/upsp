aecf46edf6cf3b53518e10a12c2110b127ced700
sravya kavuri <sravyakavuri08@gmail.com>
2023-06-26 04:22:13 -0500

    feat: added offer repo

---
+++ src/main/java/com/upspapp/repository/OfferRepository.java
@@ -0,0 +1,13 @@
+package com.upspapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.upspapp.modal.Offer;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+	Offer findByProductId(long productId);
+
+}
