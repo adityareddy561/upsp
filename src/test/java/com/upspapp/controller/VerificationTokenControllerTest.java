ec216c6508a8a8c86f68b7ef9560e6ff857c04a8
SanjanaViswanathuni <bviswan@ilstu.edu>
2023-06-30 22:10:22 -0500

    Feat:verify

--- src/test/java/com/upspapp/controller/VerificationTokenControllerTest.java
+++ src/test/java/com/upspapp/controller/VerificationTokenControllerTest.java
@@ -3,6 +3,9 @@ package com.upspapp.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +13,11 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.upspapp.UPSPApplication;
 import com.upspapp.requestDto.OtpVerificationDto;
@@ -42,4 +47,24 @@ public class VerificationTokenControllerTest {
 				ApiResponseDtoBuilder.class);
 		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 	}
+	
+	@Test
+	public void resendRegistrationToken() throws Exception {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		long id = 1l;
+		String url = URL + port + "/api/resendRegistrationToken";
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		String urlTemplate = UriComponentsBuilder.fromHttpUrl(url).queryParam("id", "{id}").encode()
+				.toUriString();
+		Map<String, Object> params = new HashMap<>();
+		params.put("id", id);
+		ResponseEntity<ApiResponseDtoBuilder> responseEntity = restTemplate.exchange(urlTemplate, HttpMethod.GET,
+				entity, ApiResponseDtoBuilder.class, params);
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+	
+	}
+	
 }
--- src/test/java/com/upspapp/serviceImpl/VerificationTokenServiceImplTest.java
+++ src/test/java/com/upspapp/serviceImpl/VerificationTokenServiceImplTest.java
@@ -1,6 +1,10 @@
 package com.upspapp.serviceImpl;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +15,10 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.upspapp.config.JwtTokenUtil;
+import com.upspapp.modal.User;
 import com.upspapp.repository.UserRepository;
 import com.upspapp.repository.VerificationTokenRepository;
+import com.upspapp.requestDto.OtpVerificationDto;
 import com.upspapp.responseDto.ApiResponseDto.ApiResponseDtoBuilder;
 import com.upspapp.service.IEmailService;
 
@@ -46,7 +52,30 @@ public class VerificationTokenServiceImplTest {
 	public void resendRegistrationToken() {
 		ApiResponseDtoBuilder apiResponseDtoBuilder = new ApiResponseDtoBuilder();
 		long id =1L;
+		User user=new User();
+		user.setId(1L);
+		user.setRole(0);
+		user.setActive(true);
+		user.setEmail("testadmin@gmail.com");
+		user.setFullName("Test Admin");
+		user.setMobileNumber("11111111");
+		user.setPassword("12345");
+		user.setCreatedAt(new Date());
+		Optional<User> optionalUser =Optional.of(user);
+		when(userRepository.findById(id)).thenReturn(optionalUser);
 		verificationTokenServiceImpl.resendRegistrationToken(id, apiResponseDtoBuilder);
 		assertTrue(apiResponseDtoBuilder.getMessage().equals("Confirmation email has been sent"));
+	}
+	
+	
+	@Test
+	public void otpVerification() {
+		ApiResponseDtoBuilder apiResponseDtoBuilder = new ApiResponseDtoBuilder();
+		OtpVerificationDto otpVerificationDto=new OtpVerificationDto();
+		otpVerificationDto.setOtp("Test");
+		otpVerificationDto.setTermsAndConditions(true);
+		otpVerificationDto.setUserId(1l);
+		verificationTokenServiceImpl.otpVerification(otpVerificationDto, apiResponseDtoBuilder, null);
+		assertTrue(apiResponseDtoBuilder.getMessage().equals("User Or OTP Not Exist !"));
 	}
 }
