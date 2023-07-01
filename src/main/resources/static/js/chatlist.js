ac2d4727406b7a20ebb846c6169da103d99af842
sravya kavuri <sravyakavuri08@gmail.com>
2023-07-01 00:22:00 -0500

    fix: chatlist

--- src/main/resources/static/js/chatlist.js
+++ src/main/resources/static/js/chatlist.js
@@ -7,73 +7,71 @@ function chatlist() {
     headers: { "Authorization": "Bearer " + localStorage.getItem('jsonToken') },
     cache: false,
     timeout: 600000,
-    success: function(data) {
+    success: function (data) {
       console.log(data.data)
       if (data.data.length > 0) {
         for (var item in data.data) {
           $('.itemList').append(
-            '<li><a id="profilesender" onClick=singleResponse(\"' + data.data[item].email.trim() + '\")>'+data.data[item].email+
+            '<li><a id="profilesender" onClick=singleResponse(\"' + data.data[item].email.trim() + '\")>' + data.data[item].email +
             '</a></li>');
 
         }
-      } 
+      }
     },
-    error : function(e) {
-     
+    error: function (e) {
+
     }
   });
 }
 function myFunction() {
-	
+
   var data = document.getElementById("count_id").value;
-  var  datacount = (parseInt(data) + 1);
-  document.getElementById("count_id").value = datacount;	
+  var datacount = (parseInt(data) + 1);
+  document.getElementById("count_id").value = datacount;
   document.getElementById("profilesender").value = "Clicked";
-  
+
 }
 //  setInterval(function(){
 //   document.getElementById("profilesender").click();	
 //    }, 1000);
 
-let receiverId = "";
-function singleResponse(email) {
-  receiverId=email
-	const msgerForm = get(".msger-inputarea");
-const msgerInput = get(".msger-input");
+function singleResponse() {
+  const msgerForm = get(".msger-inputarea");
+  const msgerInput = get(".msger-input");
 
-const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
-$('#msgtest').html("");
+  const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+  $('#msgtest').html("");
   $.ajax({
     type: "GET",
     contentType: "application/json",
-    url: "http://localhost:8181/api/messages/get/"+email+"/"+localStorage.getItem("user_details"),
+    url: "http://localhost:8181/api/messages/get/" + localStorage.getItem("seller") + "/" + localStorage.getItem("uId"),
     dataType: 'json',
     headers: { "Authorization": "Bearer " + localStorage.getItem('jsonToken') },
     cache: false,
     timeout: 600000,
-    success: function(data) {
+    success: function (data) {
       console.log(data.data)
       if (data.data.length > 0) {
         for (var item in data.data) {
-          BOT_NAME=data.data[item].sender;
-          msgText=data.data[item].message;
-          time=data.data[item].createdAt;
-          if(BOT_NAME==email){
-            appendMessage(BOT_NAME, BOT_IMG, "left", msgText,time);
-          }else{
-            appendMessage(BOT_NAME, BOT_IMG, "right", msgText,time);
+          BOT_NAME = data.data[item].sender;
+          msgText = data.data[item].message;
+          time = data.data[item].createdAt;
+          if (BOT_NAME == localStorage.getItem("uId")) {
+            appendMessage(BOT_NAME, BOT_IMG, "left", msgText, time);
+          } else {
+            appendMessage(BOT_NAME, BOT_IMG, "right", msgText, time);
           }
-          
+
         }
-      } 
-     // document.getElementById("profilesender").value = "Clicked";
+      }
+      // document.getElementById("profilesender").value = "Clicked";
     },
-    error : function(e) {
-     
+    error: function (e) {
+
     },
   });
 }
-function appendMessage(name, img, side, text,time) {
+function appendMessage(name, img, side, text, time) {
   // Simple solution for small apps
   const msgHTML = `
     <div class="msg ${side}-msg">
